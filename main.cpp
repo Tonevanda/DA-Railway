fd39d7f05361c0ce33882917347ccd9991f28ee9
Tonevanda <up202108863@up.pt>
2023-03-10 15:50:28 +0000

    Changed CMake configurations

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include "gtest/gtest.h"
+
+using namespace std;
 
 int main(int argc, char **argv) {
-    std::cout << "Running all tests..." << std::endl;
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    cout << "Hello World!";
 }
