89894c35b0bed34f245183ccf96960d76b81a417
Tonevanda <up202108863@up.pt>
2023-03-10 14:00:07 +0000

    Initial commit

---
+++ cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/tonevanda/Downloads/DA-QuickStart/lib/googletest-master/googlemock
+# Build directory: /Users/tonevanda/Downloads/DA-QuickStart/cmake-build-debug/lib/googletest-master/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
