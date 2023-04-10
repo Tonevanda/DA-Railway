76aff2b1a9d3bbf741ec5f1cfdd917226ab34ac8
Tiago Cruz <tiagocruz057@gmail.com>
2023-04-10 23:23:05 +0100

    now uses residual path

--- Graph.cpp
+++ Graph.cpp
@@ -649,11 +649,20 @@ void Graph::updateFlowResidual(Station* source, Station* target, double bottlene
 
         if(e->getDest()==currentVertex){
             e->setFlow(e->getFlow() + bottleneck);
+            e->setResidual(e->getResidual()+bottleneck);
+
+            //e->getReverse()->setResidual(e->getReverse()->getResidual() + bottleneck);
+
+            //e->getReverse()->setResidual(e->getReverse()->getResidual()+bottleneck);
             currentVertex = e->getOrig();
             //cout << "New flow: " << e->getOrig()->getName() << " até " <<  e->getDest()->getName() <<": " << e->getFlow() << endl;
         }
         else{
-            e->setResidual(e->getResidual() + bottleneck);
+            e->setResidual(e->getResidual() - bottleneck);
+
+            //e->setResidual(e->getResidual() - bottleneck);
+            //e->getReverse()->setFlow(e->getReverse()->getFlow()-bottleneck);
+
             //e->setFlow(e->getFlow() - bottleneck);
             currentVertex = e->getDest();
             //cout << "New flow Residual: " << e->getOrig()->getName() << " até " << e->getDest()->getName() <<": " << e->getResidual() << endl;
@@ -671,10 +680,7 @@ bool Graph::edmondsKarpBFSArea(Station* source, string* target){
     q.push(source);
     Station* tempTarget;
     bool firstIt = false;
-    if(*target != ""){
-        tempTarget = findStation(*target);
-    }
-    else{
+    if(*target==""){
         firstIt = true;
     }
     if(source->getAdj().size()==0&&source->getIncoming().size()==0){
@@ -685,15 +691,21 @@ bool Graph::edmondsKarpBFSArea(Station* source, string* target){
     while(!q.empty()){
         Station* u = q.front();
         q.pop();
+
         for(auto e: u->getAdj()){
             int qsize=q.size();
             testVisit(q, e, e->getDest(), e->getCapacity() - e->getFlow());
-            if(q.size()>qsize){
+            if(q.size()>qsize&&firstIt){
                 *target = e->getDest()->getName();
             }
         }
+
         for(auto edge : u->getIncoming()){
+            int qsize=q.size();
             testVisit(q, edge, edge->getOrig(), edge->getResidual());
+            if(q.size()>qsize&&firstIt){
+                *target = edge->getDest()->getName();
+            }
         }
     }
     tempTarget = findStation(*target);
